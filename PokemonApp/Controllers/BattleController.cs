f024a0df4871dccd85e22268eed34f632de0838c
Josué Brenes <josuebrenes3005@gmail.com>
2024-11-11 20:21:28 -0600

    feat: logic game

---
+++ PokemonApp/Controllers/BattleController.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using PokemonApp.Models;
+
+public class BattleController : Controller
+{
+    private DatabaseEntities db = new DatabaseEntities();
+
+    public ActionResult Index()
+    {
+        var pokemons = db.Pokemons.ToList();
+        return View(pokemons);
+    }
+
+    [HttpPost]
+    public ActionResult StartBattle(int pokemon1Id, int pokemon2Id)
+    {
+        var pokemon1 = db.Pokemons.Find(pokemon1Id);
+        var pokemon2 = db.Pokemons.Find(pokemon2Id);
+
+        if (pokemon1 == null || pokemon2 == null)
+        {
+            return HttpNotFound("One or both Pokémon not found.");
+        }
+
+        var battle = new Battle { Pokemon1 = pokemon1, Pokemon2 = pokemon2 };
+        var result = battle.StartBattle();
+
+        ViewBag.Result = result;
+        return View("BattleResult");
+    }
+}
---
+++ PokemonApp/Models/Battle.cs
@@ -0,0 +1,28 @@
+﻿using PokemonApp.Models;
+using System;
+
+public class Battle
+{
+    public Pokemon Pokemon1 { get; set; }
+    public Pokemon Pokemon2 { get; set; }
+
+    public string StartBattle()
+    {
+        Random random = new Random();
+        int pokemon1Attack = random.Next(1, 100);
+        int pokemon2Attack = random.Next(1, 100);
+
+        if (pokemon1Attack > pokemon2Attack)
+        {
+            return $"{Pokemon1.Nombre} wins the battle with an attack power of {pokemon1Attack}!";
+        }
+        else if (pokemon2Attack > pokemon1Attack)
+        {
+            return $"{Pokemon2.Nombre} wins the battle with an attack power of {pokemon2Attack}!";
+        }
+        else
+        {
+            return "It's a draw! Both Pokémon have equal attack power!";
+        }
+    }
+}
