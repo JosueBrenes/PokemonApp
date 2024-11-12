33199a919c6825c7f3d1cb39268df7a80b991603
Josué Brenes <josuebrenes3005@gmail.com>
2024-11-11 19:22:34 -0600

    Revert "feat: update nurseController"
    This reverts commit 754f346366e2bf362b37e0057cb7182ecc27a60e.

--- PokemonApp/Controllers/NurseController.cs
+++ PokemonApp/Controllers/NurseController.cs
@@ -6,46 +6,48 @@ namespace PokemonApp.Controllers
 {
     public class NurseController : Controller
     {
-        private static List<NurseRequest> nurseRequests = new List<NurseRequest>();
-
         public ActionResult Index()
         {
             var nurses = new List<Nurse>
             {
-                new Nurse { Name = "Nurse Joy", Age = 30, Region = "Kanto", ExperienceYears = 10 },
-                new Nurse { Name = "Nurse Rose", Age = 28, Region = "Johto", ExperienceYears = 8 },
-                new Nurse { Name = "Nurse Lily", Age = 35, Region = "Hoenn", ExperienceYears = 12 },
-                new Nurse { Name = "Nurse Daisy", Age = 32, Region = "Sinnoh", ExperienceYears = 9 },
-                new Nurse { Name = "Nurse Violet", Age = 29, Region = "Unova", ExperienceYears = 7 }
+                new Nurse
+                {
+                    Name = "Nurse Joy",
+                    Age = 30,
+                    Region = "Kanto",
+                    ExperienceYears = 10
+                },
+                new Nurse
+                {
+                    Name = "Nurse Rose",
+                    Age = 28,
+                    Region = "Johto",
+                    ExperienceYears = 8
+                },
+                new Nurse
+                {
+                    Name = "Nurse Lily",
+                    Age = 35,
+                    Region = "Hoenn",
+                    ExperienceYears = 12
+                },
+                new Nurse
+                {
+                    Name = "Nurse Daisy",
+                    Age = 32,
+                    Region = "Sinnoh",
+                    ExperienceYears = 9
+                },
+                new Nurse
+                {
+                    Name = "Nurse Violet",
+                    Age = 29,
+                    Region = "Unova",
+                    ExperienceYears = 7
+                }
             };
 
             return View(nurses);
-        }
-
-
-        public ActionResult CreateRequest()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        public ActionResult CreateRequest(NurseRequest request)
-        {
-            if (ModelState.IsValid)
-            {
-                request.Id = nurseRequests.Count + 1;
-                request.IsAttended = false;
-                nurseRequests.Add(request);
-                return RedirectToAction("Requests");
-            }
-
-            return View(request);
-        }
-
-
-        public ActionResult Requests()
-        {
-            return View(nurseRequests);
         }
     }
 }
