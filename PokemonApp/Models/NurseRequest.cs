b3a5df278303382de19f8d064f88ec58711cd572
Feru0110 <mugalde70964@ufide.ac.cr>
2024-11-11 18:59:54 -0600

    feat: add nurseRequest model

---
+++ PokemonApp/Models/NurseRequest.cs
@@ -0,0 +1,11 @@
+namespace PokemonApp.Models
+{
+	public class NurseRequest
+	{
+		public int Id { get; set; }
+		public string PokemonName { get; set; }
+		public string Description { get; set; }
+		public string AssignedNurse { get; set; }
+		public bool IsAttended { get; set; }
+	}
+}
