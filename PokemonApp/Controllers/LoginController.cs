9e6ad243c1fd7bb4d852ade8f929940da6197aee
Feru0110 <mugalde70964@ufide.ac.cr>
2024-11-11 18:47:14 -0600

    feat: add login controller

---
+++ PokemonApp/Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace PokemonApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
