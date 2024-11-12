3f95b64126f77cc6d4fb70f93211400c514f3186
Josué Brenes <josuebrenes3005@gmail.com>
2024-11-11 21:05:59 -0600

    fix: logic nurse

--- PokemonApp/Controllers/NurseController.cs
+++ PokemonApp/Controllers/NurseController.cs
@@ -1,11 +1,13 @@
 ﻿using PokemonApp.Models;
 using System.Collections.Generic;
+using System.Linq;
 using System.Web.Mvc;
 
 namespace PokemonApp.Controllers
 {
     public class NurseController : Controller
     {
+        private DatabaseEntities db = new DatabaseEntities(); // Conexión a la base de datos
         private static List<NurseRequest> nurseRequests = new List<NurseRequest>();
 
         public ActionResult Index()
@@ -24,21 +26,51 @@ namespace PokemonApp.Controllers
 
         public ActionResult CreateRequest()
         {
-            return View();
+            var model = new CreateRequest
+            {
+                Pokemons = db.Pokemons.ToList(),
+                Nurses = new List<Nurse>
+                {
+                    new Nurse { Name = "Nurse Joy", Age = 30, Region = "Kanto", ExperienceYears = 10 },
+                    new Nurse { Name = "Nurse Rose", Age = 28, Region = "Johto", ExperienceYears = 8 },
+                    new Nurse { Name = "Nurse Lily", Age = 35, Region = "Hoenn", ExperienceYears = 12 },
+                    new Nurse { Name = "Nurse Daisy", Age = 32, Region = "Sinnoh", ExperienceYears = 9 },
+                    new Nurse { Name = "Nurse Violet", Age = 29, Region = "Unova", ExperienceYears = 7 }
+                }
+            };
+
+            return View(model);
         }
 
         [HttpPost]
-        public ActionResult CreateRequest(NurseRequest request)
+        public ActionResult CreateRequest(CreateRequest model)
         {
             if (ModelState.IsValid)
             {
-                request.Id = nurseRequests.Count + 1;
-                request.IsAttended = false;
-                nurseRequests.Add(request);
+                var nurseRequest = new NurseRequest
+                {
+                    Id = nurseRequests.Count + 1,
+                    PokemonName = model.PokemonName, 
+                    Description = model.Description,
+                    AssignedNurse = model.AssignedNurse, 
+                    IsAttended = false 
+                };
+
+                nurseRequests.Add(nurseRequest);
                 return RedirectToAction("Requests");
             }
 
-            return View(request);
+            model.Pokemons = db.Pokemons.ToList();
+            model.Nurses = new List<Nurse>
+            {
+                new Nurse { Name = "Nurse Joy", Age = 30, Region = "Kanto", ExperienceYears = 10 },
+                new Nurse { Name = "Nurse Rose", Age = 28, Region = "Johto", ExperienceYears = 8 },
+                new Nurse { Name = "Nurse Lily", Age = 35, Region = "Hoenn", ExperienceYears = 12 },
+                new Nurse { Name = "Nurse Daisy", Age = 32, Region = "Sinnoh", ExperienceYears = 9 },
+                new Nurse { Name = "Nurse Violet", Age = 29, Region = "Unova", ExperienceYears = 7 }
+            };
+
+            return View(model);
         }
 
         public ActionResult Requests()
---
+++ PokemonApp/Models/CreateRequest.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace PokemonApp.Models
+{
+    public class CreateRequest
+    {
+        public string PokemonName { get; set; } // Nombre del Pokémon seleccionado
+        public string Description { get; set; } // Descripción del problema
+        public string AssignedNurse { get; set; } // Enfermera asignada
+        public List<Pokemon> Pokemons { get; set; } // Lista de Pokémon disponibles
+        public List<Nurse> Nurses { get; set; } // Lista de enfermeras disponibles
+    }
+}
